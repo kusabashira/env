1c316a2b511537d54803889be8306ea65dee3d6a
kusabashira <kusabashira227@gmail.com>
2015-10-31 05:35:07 +0900

    Add vim-plugins and dependencies

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -1,1 +1,24 @@
 remote_file "/home/wara/.vimrc"
+remote_file "/home/wara/.editorconfig"
+
+directory   "/home/wara/.vim"
+remote_file "/home/wara/.vim/input"
+
+define :vim_plugin, set: "bundle" do
+  name, set = params[:name], params[:set]
+  url = "https://github.com/#{name}.git"
+  dst = "/home/wara/.vim/#{set}/#{name.sub(/^.+?\//, '')}"
+  git dst do
+    repository url
+    not_if "test -e #{dst}"
+  end
+end
+
+vim_plugin "sunaku/vim-unbundle"
+vim_plugin "thinca/vim-quickrun"
+vim_plugin "tyru/open-browser.vim"
+vim_plugin "kannokanno/previm"
+vim_plugin "editorconfig/editorconfig-vim"
+
+vim_plugin "fatih/vim-go"           do set "ftbundle/go" end
+vim_plugin "hotchpotch/perldoc-vim" do set "ftbundle/perl" end
