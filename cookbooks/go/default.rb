75a9b1c62cb78bb552cd5d5c69dfdf66c5f78887
kusabashira <kusabashira227@gmail.com>
2015-10-31 16:52:37 +0900

    Download development tools for go

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,2 +1,18 @@
 directory "/home/wara/work"
 directory "/home/wara/work/go"
+
+define :go_get do
+  uri = params[:name]
+  dst = "/home/wara/work/go/src/#{uri}"
+  execute "go get #{uri}" do
+    command "GOPATH=/home/wara/work/go go get #{uri}"
+    not_if  "test -e #{dst}"
+  end
+end
+
+go_get 'golang.org/x/tools/cmd/gorename'
+go_get 'github.com/rogpeppe/godef'
+go_get 'github.com/nsf/gocode'
+go_get 'github.com/golang/lint/golint'
+go_get 'github.com/bradfitz/goimports'
+go_get 'github.com/jteeuwen/go-bindata'
