4d2ea253dcd40b9fcf8a50c4cdfcea48c350d54b
kusabashira <kusabashira227@gmail.com>
2015-12-07 16:38:50 +0900

    Add scripts

--- cookbooks/shell/default.rb
+++ cookbooks/shell/default.rb
@@ -7,3 +7,8 @@ remote_file "/home/wara/.bash/er.bash"
 remote_file "/home/wara/.bash/cdr.bash"
 remote_file "/home/wara/.bash/cdf.bash"
 remote_file "/home/wara/.bash/cdp.bash"
+
+directory   "/home/wara/bin"
+remote_file "/home/wara/bin/ej"
+remote_file "/home/wara/bin/je"
+remote_file "/home/wara/bin/monico"
